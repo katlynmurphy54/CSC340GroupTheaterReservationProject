b8b29dc416b49f98be16d45baee3b2986ff2f300
Grayson <graycar23@gmail.com>
2020-11-03 12:14:54 -0500

    Register Button

--- TheaterReservation/Form1.cs
+++ TheaterReservation/Form1.cs
@@ -16,5 +16,10 @@ namespace TheaterReservation
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

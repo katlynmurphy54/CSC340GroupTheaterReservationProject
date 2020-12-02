47b8bd18fb6e9d8751b590172e2f52d8ee09f74f
Katlyn Murphy <73905914+katlynmurphy54@users.noreply.github.com>
2020-12-02 13:25:22 -0500

    Update EventInfo.designer.cs

--- TheaterReservation/EventInfo.designer.cs
+++ TheaterReservation/EventInfo.designer.cs
@@ -32,31 +32,24 @@
             this.label11 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.label11);
             this.panel2.Controls.Add(this.label10);
             this.panel2.Controls.Add(this.pictureBox4);
-            this.panel2.Controls.Add(this.label9);
-            this.panel2.Controls.Add(this.comboBox1);
             this.panel2.Controls.Add(this.label8);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(12, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1086, 472);
+            this.panel2.Size = new System.Drawing.Size(653, 350);
             this.panel2.TabIndex = 2;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
@@ -64,7 +57,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(275, 38);
+            this.label11.Location = new System.Drawing.Point(274, 17);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(114, 17);
             this.label11.TabIndex = 13;
@@ -73,90 +66,58 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(122, 287);
+            this.label10.Location = new System.Drawing.Point(109, 252);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(74, 13);
             this.label10.TabIndex = 12;
-            this.label10.Text = "December 4-6";
+            this.label10.Text = "December 4th";
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = global::TheaterReservation.Properties.Resources._091019_Hamilton_JoanMarcus_02;
-            this.pictureBox4.Location = new System.Drawing.Point(24, 107);
+            this.pictureBox4.Location = new System.Drawing.Point(24, 72);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(255, 177);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 11;
             this.pictureBox4.TabStop = false;
             // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(285, 247);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(272, 13);
-            this.label9.TabIndex = 10;
-            this.label9.Text = "Please choose a date below to view seating availability: ";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "December 4th, 2020",
-            "December 5th, 2020",
-            "December 6th, 2020"});
-            this.comboBox1.Location = new System.Drawing.Point(288, 263);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 9;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(285, 107);
+            this.label8.Location = new System.Drawing.Point(285, 72);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(252, 65);
+            this.label8.Size = new System.Drawing.Size(247, 65);
             this.label8.TabIndex = 8;
             this.label8.Text = "Featuring Daveed Diggs and Jonathan Groff,\r\nthis musical is coming to Lexington f" +
-    "or 3 nights only! \r\nGeneral Pricing: $59.99 per ticket\r\nVIP Pricing: $49.99 per " +
-    "ticket\r\nMax: 4 tickets per person";
+    "or 1 night only! \r\nGeneral Pricing: $59.99 per ticket\r\nVIP Pricing: $49.99 per t" +
+    "icket\r\nMax: 4 tickets per person";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(109, 89);
+            this.label1.Location = new System.Drawing.Point(109, 54);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 15);
             this.label1.TabIndex = 4;
             this.label1.Text = "Hamilton";
             // 
-            // label2
+            // button1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(695, 38);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(244, 17);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "Seating Availability for: December 4th";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::TheaterReservation.Properties.Resources.Picture1;
-            this.pictureBox1.Location = new System.Drawing.Point(668, 89);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(356, 356);
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.button1.Location = new System.Drawing.Point(297, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 57);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Make Reservation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // EventInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1110, 496);
+            this.ClientSize = new System.Drawing.Size(711, 496);
             this.Controls.Add(this.panel2);
             this.Name = "EventInfo";
             this.Text = "Lexington Theatre Events Page";
@@ -164,7 +125,6 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -175,11 +135,8 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }

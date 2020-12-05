2c26dfe72778bb66e25e23d16f51e4b8afb21b04
Katlyn Murphy <73905914+katlynmurphy54@users.noreply.github.com>
2020-12-05 00:59:15 -0500

    Update EventInfo.cs

--- TheaterReservation/EventInfo.cs
+++ TheaterReservation/EventInfo.cs
@@ -14,6 +14,7 @@ namespace TheaterReservation
     {
 
         public static String eventType = "";
+        public static String eventDate = "";
 
 
         public EventInfo()
@@ -27,37 +28,50 @@ namespace TheaterReservation
             {
                 label1.Text = eventType;
                 pictureBox4.Image = TheaterReservation.Properties.Resources._091019_Hamilton_JoanMarcus_02;
-                label10.Text = "December 4th";
+                label10.Text = "December 4th, 2020";
                 label8.Text = "Featuring Daveed Diggs and Jonathan Groff, this musical is coming" +
-                    "\nto Lexington for 1 night only from December 4th through December 6th!" +
-                    "\nGeneral Pricing: $59.99 per ticket " +
-                    "\nVIP Pricing: $49.99 per ticket " +
+                    "\nto Lexington for 1 night only on December 4th!" +
+                    "\nGeneral Pricing: $50.00 per ticket " +
+                    "\nVIP Pricing: $35.00 per ticket " +
                     "\nMax: 4 tickets per person";
+                eventDate = "2020-12-04";
             }
             else if (eventType == "Wizard of Oz")
             {
                 label1.Text = eventType;
                 pictureBox4.Image = TheaterReservation.Properties.Resources.a0d0217a_79bd_47b4_b894_531f31e504e1;
-                label10.Text = "December 18th";
+                label10.Text = "December 18th, 2020";
                 label8.Text = "Featuring Lucy Durack and Samantha Dodemaide, this musical is coming" +
-                    "\nto Lexington for 1 night only from December 18th through December 20th!" +
-                    "\nGeneral Pricing: $39.99 per ticket " +
-                    "\nVIP Pricing: $29.99 per ticket " +
+                    "\nto Lexington for 1 night only on December 18th!" +
+                    "\nGeneral Pricing: $50.00 per ticket " +
+                    "\nVIP Pricing: $35.00 per ticket " +
                     "\nMax: 4 tickets per person";
+                eventDate = "2020-12-18";
             }
             else if (eventType == "Romeo and Juliet")
             {
                 label1.Text = eventType;
                 pictureBox4.Image = TheaterReservation.Properties.Resources._0727_romeojuliet;
-                label10.Text = "December 25th";
+                label10.Text = "December 25th, 2020";
                 label8.Text = "Featuring Orlando Bloom and Condola Rashad, this musical is coming" +
-                    "\nto Lexington for 1 night only from December 25th through December 27th!" +
-                    "\nGeneral Pricing: $49.99 per ticket " +
-                    "\nVIP Pricing: $39.99 per ticket " +
+                    "\nto Lexington for 1 night only on December 25th!" +
+                    "\nGeneral Pricing: $50.00 per ticket " +
+                    "\nVIP Pricing: $35.00 per ticket " +
                     "\nMax: 4 tickets per person";
+                eventDate = "2020-12-25";
             }
-
-
+            else if (eventType == "Peter Pan")
+            {
+                label1.Text = eventType;
+                pictureBox4.Image = TheaterReservation.Properties.Resources.peterpan__1_;
+                label10.Text = "October 2nd, 2020";
+                label8.Text = "Featuring Cathy Rigby and Ryan Francis, this musical is coming" +
+                    "\nto Lexington for 1 night only on October 2nd!" +
+                    "\nGeneral Pricing: $50.00 per ticket " +
+                    "\nVIP Pricing: $35.00 per ticket " +
+                    "\nMax: 4 tickets per person";
+                eventDate = "2020-10-02";
+            }
         }
 
         private void panel2_Paint(object sender, PaintEventArgs e)
@@ -81,21 +95,28 @@ namespace TheaterReservation
             {
                 ReserveSeats addseats = new ReserveSeats();
                 addseats.eventName = eventType;
-                addseats.eventDate = "December 4th";
+                addseats.eventDate = eventDate;
                 addseats.ShowDialog();
             }
             else if (eventType == "Wizard of Oz")
             {
                 ReserveSeats addseats = new ReserveSeats();
                 addseats.eventName = eventType;
-                addseats.eventDate = "December 18th";
+                addseats.eventDate = eventDate;
                 addseats.ShowDialog();
             }
             else if (eventType == "Romeo and Juliet")
             {
                 ReserveSeats addseats = new ReserveSeats();
                 addseats.eventName = eventType;
-                addseats.eventDate = "December 25th";
+                addseats.eventDate = eventDate;
+                addseats.ShowDialog();
+            }
+            else if (eventType == "Peter Pan")
+            {
+                ReserveSeats addseats = new ReserveSeats();
+                addseats.eventName = eventType;
+                addseats.eventDate = eventDate;
                 addseats.ShowDialog();
             }
         }
